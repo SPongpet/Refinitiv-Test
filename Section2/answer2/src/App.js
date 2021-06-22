e91200b1943f35523d29761f30eae624ef899de4
PONGPET SANGKHANGAM <pongpet@magicboxasia.com>
2021-06-22 15:16:29 +0700

    update section 2 answer 2

--- Section2/answer2/src/App.js
+++ Section2/answer2/src/App.js
@@ -1,23 +1,51 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [categories, setCategories] = useState([])
+  const [defaultData, setDefault] = useState([])
+
+  useEffect(() => {
+    const setUp = async () => {
+      const dataJson = await fetch('https://api.publicapis.org/categories')
+      const data = await dataJson.json()
+      setCategories(data)
+      setDefault(data)
+    }
+    setUp()
+  }, [])
+
+  const filterCategories = e => {
+    const filterData = defaultData.filter(item => item.toLowerCase().includes(e))
+    if (filterData.length > 0) {
+      setCategories(filterData)
+    }
+
+    if(e === '') {
+      setCategories(defaultData)
+    }
+  }
 
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      <div>
+        <input onChange={e => filterCategories(e.target.value)}/>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>No.</th>
+            <th>Categories</th> 
+          </tr>
+          {categories.map((item, index) => {
+            return (
+              <tr>
+                <td>{index}</td>
+                <td>{item}</td>
+              </tr>
+            )
+          })}
+        </table>
+      </div>
     </div>
   );
 }
